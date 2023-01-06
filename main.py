bc5a75ec6fa148f7d5abd4604b5d43d96c90776e
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-06 14:31:16 -0500

    23.7.4 part c

--- main.py
+++ main.py
@@ -8,15 +8,33 @@ app.secret_key = 'SIH*v-6u)c>q<;;h&);cRw,1E_CO8>'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pass
+        
+        guess=int(request.form['guess'])
+        magic_number = session['magic_number']
+        high = session['high_value']
+        low= session['low_value']
+
+        if guess < low or guess > high:
+            message = "Invalid entry: Please enter a number within the range"
+        else:
+            if guess == magic_number:
+                message = "CORRECT!"
+            elif guess < magic_number:
+                message = "Your guess is too low"
+                session['low_value']= guess
+            elif guess > magic_number:
+                message = "your guess is too high"
+                session['high_value']= guess
+
+
     else:
         low_value = 1
         high_value = 25
         session["magic_number"] = random.randint(low_value, high_value)
         session["high_value"] = high_value
         session["low_value"] = low_value
-
         message = ''
+
 
     return render_template('index.html', message = message)
 
